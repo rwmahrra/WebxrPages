cb216e60c30026ccab7f83333d5ef52404e238cf
rwmahrra <49766063+rwmahrra@users.noreply.github.com>
2020-11-10 10:57:40 -0600

    Update app.js

--- start/lecture2_5/app.js
+++ start/lecture2_5/app.js
@@ -26,7 +26,14 @@ class App{
 		
         const geometry = new THREE.TorusKnotBufferGeometry( 0.8, 0.3, 120, 16 ); 
         
-        const material = new THREE.MeshBasicMaterial( { color: 0xFF0000 });
+        //takes a color value - no lights
+        const material = new THREE.MeshBasicMaterial( { color: 0xFFFF00 });
+
+
+
+
+
+
 
         this.mesh = new THREE.Mesh( geometry, material );
         
