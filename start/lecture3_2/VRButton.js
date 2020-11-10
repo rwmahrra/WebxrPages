b11ec61931b033039d8cb02b0a55c40ac6e22fb2
rwmahrra <49766063+rwmahrra@users.noreply.github.com>
2020-11-10 16:00:41 -0600

    Update VRButton.js

--- start/lecture3_2/VRButton.js
+++ start/lecture3_2/VRButton.js
@@ -10,7 +10,13 @@ class VRButton{
         this.renderer = renderer;
         
         if ( 'xr' in navigator ) {
-            
+            const button = document.createElement('button');
+            button.style.display = 'none';
+            button.style.height = '40px';
+            document.body.appendChild(button);
+
+            navigator.xr.isSessionSupported('immersive-vr').then((supported) => {supported ? this.showEnterVR(button) : this.showWebXRNotFound(button);
+            });
 		} else {
             
 		}
@@ -18,7 +24,15 @@ class VRButton{
     }
 
 	showEnterVR( button ) {
-    
+        this.stylizeElement(button, true, 30, true);
+
+        button.style.display = '';
+        button.style.right = '20px';
+        button.style.width = '80px';
+        button.style.cursor = 'pointer';
+        button.innerHTML = '<i class="fas fa-vr-cardboard></i>';
+
+        
     }
 
     disableButton( button ) {
@@ -34,7 +48,17 @@ class VRButton{
     }
 
     showWebXRNotFound( button ) { 
-    
+        this.stylizeElement(button, false);
+        this.disableButton(button);
+
+        button.style.display = '';
+        button.style.width = '100%';
+        button.style.right = '0px';
+        button.style.bottom = '0px';
+        button.style.border = '';
+        button.style.opacity = '1';
+        button.style.fontSize = '13px';
+        button.textContent = 'VR NOT SUPPORTED';
     }
 
     stylizeElement( element, green = true, fontSize = 13, ignorePadding = false ) {
